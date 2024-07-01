d2600a3c7ea563224877dd74f26bb3a7015ad7cc
Billy Wu <biyi.wu@gmail.com>
2024-07-01 21:52:34 +1000

    Update ocr.py

--- OCR/ocr.py
+++ OCR/ocr.py
@@ -1,9 +1,6 @@
 import pytesseract
 from PIL import Image
 
-# Specify the path to the Tesseract executable (usually not needed on macOS if installed via Homebrew)
-# pytesseract.pytesseract.tesseract_cmd = r'/usr/local/bin/tesseract'
-
 def ocr_image(image_path):
     # Open an image file
     with Image.open(image_path) as img:
